fc5114e18c399d25efd5f8112899ae4241fbf544
Eric-Pessoa <pessoa.eric@gmail.com>
2024-03-03 20:37:28 -0300

    [chore] installing vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+//Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
+//Instantiate routes, vuetify and pinia
+const app = createApp(App).use(vuetify)
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
