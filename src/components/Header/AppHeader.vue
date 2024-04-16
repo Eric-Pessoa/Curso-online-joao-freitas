e283e3fe4a7210b9df6585d98b6efe35c03a5d14
Eric Pessoa <61791018+Eric-Pessoa@users.noreply.github.com>
2024-04-16 14:50:06 -0300

    stable version of header

--- src/components/Header/AppHeader.vue
+++ src/components/Header/AppHeader.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <v-app-bar color="#035E7B" :absolute="true" :elevation="2">
+  <v-app-bar color="#090809" :elevation="2">
     <template v-slot:prepend>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
     </template>
     <v-app-bar-title>Espaço da música</v-app-bar-title>
-    <v-btn icon>
-      <v-icon>mdi-logout</v-icon>
-    </v-btn>
+    <template v-slot:append>
+      <v-btn icon>
+        <v-icon>logout</v-icon>
+      </v-btn>
+    </template>
   </v-app-bar>
 </template>
 
