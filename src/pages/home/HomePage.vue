6e20bd3a9bf8b4c8024b6beb847ef0752435c52d
Eric Pessoa <61791018+Eric-Pessoa@users.noreply.github.com>
2024-04-23 17:10:34 -0300

    [feat] video-toolbar static progress

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,11 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const mockedListLessons = ref([
+  { title: 'aula 1', subtitle: '1 subtítulo' },
+  { title: 'aula 2', subtitle: '2 subtítulo' },
+  { title: 'aula 3', subtitle: '3 subtítulo' },
+  { title: 'aula 4', subtitle: '4 subtítulo' },
+  { title: 'aula 5', subtitle: '5 subtítulo' },
+  { title: 'aula 6', subtitle: '6 subtítulo' },
+  { title: 'aula 7', subtitle: '7 subtítulo' },
+  { title: 'aula 8', subtitle: '8 subtítulo' },
+  { title: 'aula 9', subtitle: '9 subtítulo' },
+  { title: 'aula 10', subtitle: '10 subtítulo' },
+  { title: 'aula 11', subtitle: '11 subtítulo' },
+  { title: 'aula 12', subtitle: '12 subtítulo' },
+])
+
+const paginatorModel = ref(1);
+</script>
 
 <template>
   <div id="main-container">
     <div id="video-player">div 1</div>
     <div id="video-toolbar">
       <div id="title">Teste</div>
-      <div>Teste</div>
+      <div id="lessons-list">
+        <v-list lines="two">
+          <v-list-item
+            v-for="n in mockedListLessons"
+            :key="n.title"
+            :title="n.title"
+            :subtitle="n.subtitle"
+          ></v-list-item>
+        </v-list>
+      </div>
+      <div id="paginator">
+        <v-pagination v-model="paginatorModel" :length="2" show-first-last-page></v-pagination>
+      </div>
     </div>
   </div>
 </template>
@@ -31,7 +62,7 @@
   #video-toolbar {
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: space-between;
     align-items: center;
     flex: 1 1 300px;
     background-color: green;
@@ -44,6 +75,18 @@
       width: 100%;
       height: 50px;
       background-color: mediumslateblue;
+    }
+    #lessons-list {
+      width: 100%;
+    }
+    #paginator {
+      display: flex;
+      width: 100%;
+      height: 50px;
+      background-color: mediumslateblue;
+      border-radius: 0 0 15px 15px;
+      justify-content: center;
+      align-items: center;
     }
   }
 }
