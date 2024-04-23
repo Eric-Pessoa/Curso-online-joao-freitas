8f5feb855882d377964acf6dce4eac4dad14d57a
Eric Pessoa <61791018+Eric-Pessoa@users.noreply.github.com>
2024-04-23 16:21:31 -0300

    [feat] initial layout of home page

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,10 +1,50 @@
 <script setup lang="ts"></script>
 
-<template><div id="main-container">teste</div></template>
+<template>
+  <div id="main-container">
+    <div id="video-player">div 1</div>
+    <div id="video-toolbar">
+      <div id="title">Teste</div>
+      <div>Teste</div>
+    </div>
+  </div>
+</template>
 
 <style scoped>
 #main-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   height: 100%;
   width: 100%;
+  padding: 15px;
+  background-color: #ededed;
+  column-gap: 10px;
+  #video-player {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 3 2 600px;
+    background-color: maroon;
+    border-radius: 15px;
+  }
+  #video-toolbar {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    flex: 1 1 300px;
+    background-color: green;
+    border-radius: 15px;
+    #title {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 15px 15px 0 0;
+      width: 100%;
+      height: 50px;
+      background-color: mediumslateblue;
+    }
+  }
 }
 </style>
