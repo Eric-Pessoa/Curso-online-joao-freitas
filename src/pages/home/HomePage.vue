381887fac5a75caa51f18595c7e670887c4e3f9e
Eric-Pessoa <pessoa.eric@gmail.com>
2024-08-14 14:50:57 -0300

    [refactor] usei classes em vez de id pra estilização

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -22,17 +22,17 @@ function clickLesson(videoTitle: string) {
 </script>
 
 <template>
-  <div id="main-container">
-    <div id="video-player">
+  <div class="main-container">
+    <div class="video-player">
       <video class="video-player-window" controls>
         <source src="movie.mp4" type="video/mp4" />
         <source src="movie.ogg" type="video/ogg" />
         Seu navegador não suporta a funcionalidade de vídeo.
       </video>
     </div>
-    <div id="video-toolbar">
-      <div id="title">Violão básico</div>
-      <div id="lessons-list">
+    <div class="video-toolbar">
+      <div class="title">Violão básico</div>
+      <div class="lessons-list">
         <v-list lines="two">
           <v-list-item
             v-for="n in mockedListLessons"
@@ -43,13 +43,13 @@ function clickLesson(videoTitle: string) {
           ></v-list-item>
         </v-list>
       </div>
-      <div id="footer"></div>
+      <div class="footer"></div>
     </div>
   </div>
 </template>
 
 <style scoped>
-#main-container {
+.main-container {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -58,29 +58,27 @@ function clickLesson(videoTitle: string) {
   padding: 15px;
   background-color: #ededed;
   column-gap: 10px;
-  #video-player {
+  .video-player {
     display: flex;
-    justify-content: center;
-    align-items: center;
     flex: 3 2 600px;
     border-radius: 15px;
     .video-player-window {
       width: 100%;
       height: 100%;
-      border-radius: 15px;
+      border-radius: inherit;
     }
   }
-  #video-toolbar {
+  .video-toolbar {
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+    /* justify-content: space-between;
+    align-items: center; */
     flex: 1 0 250px;
     min-width: 350px;
     max-height: 720px;
     background-color: #f9f1f1;
     border-radius: 15px;
-    #title {
+    .title {
       display: flex;
       justify-content: center;
       align-items: center;
@@ -92,7 +90,7 @@ function clickLesson(videoTitle: string) {
       background-color: #035e7b;
       color: #f9f1f1;
     }
-    #lessons-list {
+    .lessons-list {
       width: 100%;
       flex: 1;
       overflow-y: scroll;
@@ -104,12 +102,11 @@ function clickLesson(videoTitle: string) {
         }
       }
     }
-    #footer {
+    .footer {
       border-radius: 0 0 15px 15px;
       width: 100%;
       height: 40px;
       background-color: #035e7b;
-      color: #f9f1f1;
     }
   }
 }
